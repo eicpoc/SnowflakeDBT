354c6e540a4dc88b635a149cd41b1d75a7a5a6d4
Snowflake POC <pocsnowflakedbt@gmail.com>
2026-06-12 14:12:45 +0000

    daily_weather model created

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -3,16 +3,8 @@ with cte as (
         to_timestamp(started_at) as started_at,
         date(to_timestamp(started_at)) as date_started_at,
         hour(to_timestamp(started_at)) as hour_started_at,
-        case when lower(dayname(to_timestamp(started_at))) in ('sat','sun')
-            then 'weekend'
-            else 'businessday' end as day_type,
-        case when month(to_timestamp(started_at)) in (12,1,2)
-            then 'winter'
-            when month(to_timestamp(started_at)) in (3,4,5)
-            then 'spring'
-            when month(to_timestamp(started_at)) in (6,7,8)
-            then 'summer'
-            else 'autumn' end as station_of_year
+        {{get_day_type('started_at')}} as day_type,
+        {{get_season('started_at')}} as station_of_year
     from {{ source('demo', 'bike') }}
 
 )
