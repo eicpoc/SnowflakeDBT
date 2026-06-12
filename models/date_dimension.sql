375b176d1392b70809a29d3889de987906ecdb71
Snowflake POC <pocsnowflakedbt@gmail.com>
2026-06-12 13:41:40 +0000

    date dimension model

---
+++ models/date_dimension.sql
@@ -0,0 +1,20 @@
+with cte as (
+    select
+        to_timestamp(started_at) as started_at,
+        date(to_timestamp(started_at)) as date_started_at,
+        hour(to_timestamp(started_at)) as hour_started_at,
+        case when lower(dayname(to_timestamp(started_at))) in ('sat','sun')
+            then 'weekend'
+            else 'businessday' end as day_type,
+        case when month(to_timestamp(started_at)) in (12,1,2)
+            then 'winter'
+            when month(to_timestamp(started_at)) in (3,4,5)
+            then 'spring'
+            when month(to_timestamp(started_at)) in (6,7,8)
+            then 'summer'
+            else 'autumn' end as station_of_year
+    from {{ source('demo', 'bike') }}
+
+)
+
+select * from cte
